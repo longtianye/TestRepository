5446df6d257b51f22233e0e122bb71ed053dd4e0
wyf <wyf@wyf.local>
2012-04-24 06:24:34 +0800

    Initial Commit

---
+++ TestTabNav/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestTabNav
+//
+//  Created by wyf on 12-4-24.
+//  Copyright (c) 2012年 WYF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TestTabNav/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TestTabNav
+//
+//  Created by wyf on 12-4-24.
+//  Copyright (c) 2012年 WYF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
